18902f2252c719699b79a2af99db4f393676e576
gajendra <sabanegajendra123@gmail.com>
2021-12-01 21:21:07 +0530

    First commit. Completed till social icons

---
+++ demo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Demo
+{
+    public static void main(String[] args)
+    {
+        ArrayList x = new ArrayList();
+        x.add(140);
+        x.add(60);
+        x.add(120);
+        x.add(82);
+        x.add(180);
+        x.add(40);
+        System.out.println(x);
+    }
+}
